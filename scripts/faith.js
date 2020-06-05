08d356266972732d02b285be9f3d4752a43f9b7e
drizzt401 <faithudoh71@gmail.com>
2020-06-05 16:43:45 +0100

    Update faith.js

--- scripts/faith.js
+++ scripts/faith.js
@@ -2,7 +2,7 @@ function helloWorld(){
 var email = "faithudoh71@gmail.com";
 var fullName = "Udoh Faith";
 var hngID = "HNG-05995";
-var output = `Hello World, this is ${fullName} with HNGi7 ID ${hngID} using Javascript for stage 2 task and ${email}`
+var output = `Hello World, this is ${fullName} with HNGi7 ID ${hngID} and email ${email} using Javascript for stage 2 task`
 return (output)
 }
 console.log(helloWorld());
